a907943debc4c3ff50623f6d7e0f7f80c5adaa7c
pylover <vahid.mardani@gmail.com>
2021-08-09 17:57:26 +0430

    Mockup for /docs/index.json and /jsdist/index.json

--- server.py
+++ server.py
@@ -1,8 +1,19 @@
 #! /usr/bin/env python3
-from yhttp import Application, text
+from yhttp import Application, json, validate
 
 
 app = Application()
+
+
+@app.route('/(?:docs|jsdist)/index.json')
+@validate(fields=dict(
+    count=dict(type_=int),
+))
+@json
+def get(req, *, count=5):
+    return list(f'0.{i}' for i in range(int(count), 0, -1))
+
+
 app.staticdirectory('/', 'public', default=True, fallback=True)
 app.ready()
 try:
